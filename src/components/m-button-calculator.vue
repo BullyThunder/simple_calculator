4994195363f6ba327bef6c70b2a4f9f711ec0c0d
Saiko Ruslan <vamosbarca2407@gmail.com>
2024-10-10 14:06:29 +0300

    version 1.0

--- src/components/m-button-calculator.vue
+++ src/components/m-button-calculator.vue
@@ -16,7 +16,7 @@
       <button class="calculator__btn" @click="click_to_number(7)">7</button>
       <button class="calculator__btn" @click="click_to_number(8)">8</button>
       <button class="calculator__btn" @click="click_to_number(9)">9</button>
-      <button class="calculator__btn" >.</button>
+      <button class="calculator__btn" @click="to_fraction">.</button>
       <button class="calculator__btn" @click="click_to_number(0)">0</button>
       <button class="calculator__btn calculator__equals" @click = "calculate">=</button>
     </div>
@@ -30,30 +30,61 @@ const store = useStore();
 
 const emit = defineEmits(['custom_number']);
 const result = ref(0);
-const first_Number = ref(null);
-const second_Number = ref(null);
+const fraction = ref(0);
+const first_Number = ref(0);
+const second_Number = ref(0);
+const first_fraction = ref(false);
+const second_fraction = ref(false);
+const is_next_operation = ref(false)
 const operation = ref(null);
 const state_of_operation = ref(false);
+const to_fraction = () =>{
+  if(!first_fraction.value && !first_Number.value.toString().includes('.')){
+  first_Number.value += '.';
+  emit('custom_number', first_Number.value);
+  first_fraction.value = true;
+  }
+  else if(!second_fraction.value && !is_next_operation.value && ! second_Number.value.toString().includes('.')){
+    second_Number.value +='.';
+    emit('custom_number', second_Number.value);
+    second_fraction.value = true;
+    
+  } 
+  if(is_next_operation.value && ! second_Number.value.toString().includes('.')){
+    second_Number.value += '.';
+    emit('custom_number', second_Number.value);
+  }
+}
 const click_to_number = (key) =>{
   if(!state_of_operation.value ){
-    if(first_Number.value !== null){
+    if(first_fraction.value){
+      first_Number.value =  first_Number.value.toString() + key.toString();
+    }
+   else if(first_Number.value !== 0){
       first_Number.value = first_Number.value * 10 + key;
     }
     else {
-      first_Number.value= key;
+      first_Number.value = key;
     }
     emit('custom_number', first_Number.value);
+    console.log(first_Number.value);
   }
+  
   else if(state_of_operation.value){
-    if(second_Number.value !== null){
+    if(second_fraction.value || is_next_operation.value){
+      second_Number.value = second_Number.value.toString() + key.toString(); 
+    }
+   else if(second_Number.value !== 0){
       second_Number.value = second_Number.value * 10 + key;
     }
     else{
       second_Number.value= key;
     }
     emit('custom_number', second_Number.value);
+    console.log(second_Number.value);
   }
 }
+
 const set_operation = (symb) =>{
   if(first_Number.value !== null){
    operation.value = symb;
@@ -61,6 +92,8 @@ const set_operation = (symb) =>{
   }
 }
 const calculate = () => {
+  first_Number.value = parseFloat( first_Number.value);
+  second_Number.value = parseFloat(second_Number.value);
   if(first_Number.value !==null && second_Number.value !==null && state_of_operation.value === true){
   switch(operation.value){
     case '+':
@@ -72,21 +105,26 @@ const calculate = () => {
     case '*':
     result.value= first_Number.value * second_Number.value;
     break;
-    case '+':
+    case '/':
     result.value =  first_Number.value / second_Number.value;
     break;  
     }
     emit('custom_number',result.value);
     state_of_operation.value = false;
-    first_Number.value = result.value;
-    second_Number.value = null;
+    first_fraction.value = false;
+    second_fraction.value = false;
+    first_Number.value = result.value.toString();
+    second_Number.value = '';
+    is_next_operation.value=true;
   }
-} 
+}
 const reset = () =>{
   first_Number.value = null;
   second_Number.value = null;
   operation.value = null;
   result.value = null;
+  first_fraction.value = false;
+  second_fraction.value = false;
   state_of_operation.value = false;
   emit('custom_number',0);
 }
